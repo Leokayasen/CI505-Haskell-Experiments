e8076f7d46df8309ab1577dd2f4f382e1f4b5efd
Kai <74490542+Leokayasen@users.noreply.github.com>
2025-10-10 14:59:17 +0100

    Add files via upload

--- basics/demoScript.hs
+++ basics/demoScript.hs
@@ -2,3 +2,14 @@ square x = x * x
 cube x = x * x * x
 factorial n = product [1..n]
 average ns = sum ns `div` length ns
+
+total x y = add x y
+ where {add a b = a + b; square c = c * c}
+
+add2 = (\x -> x + 2)
+addToList :: [Int] -> [Int]
+addToList x = map (add2) x
+
+data Bool = True | False
+data Colour = Red | Blue | Green
+data Customer = Customer { id :: Int, name :: String, address :: [String], serviceLevel :: Colour }
